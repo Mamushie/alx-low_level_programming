7940c07cde46acf4fa0bc482fc3dab781af380fc
Mamushie <wawerumushie@gmail.com>
2021-10-18 18:21:56 +0300

    Update files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,47 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
  *
- * @filename: file name
- * @letters: size of character buffer
- * Return: letters read and printed or 0 if anything goes wrong
+ * Return: the number of letters printed, or 0 if it failed
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, b;
-	char *buffer;
+	int fd;
+	int s, t;
+	char *buf;
 
-	if (!filename || letters < 1)
+	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	b = read(fd, buffer, letters);
-	if (b == -1)
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
+	s = read(fd, buf, letters);
+	if (s < 0)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
-	write(STDOUT_FILENO, buffer, b);
+	buf[s] = '\0';
+
 	close(fd);
-	free(buffer);
-	return (b);
+
+	t = write(STDOUT_FILENO, buf, s);
+	if (t < 0)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	return (t);
 }
