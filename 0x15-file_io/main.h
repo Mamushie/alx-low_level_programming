9ed5844be95644b46663f6d503d342fbb66c2c77
Mamushie <wawerumushie@gmail.com>
2021-10-18 18:42:43 +0300

    Update files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,11 @@
 #include <fcnt1.h>
 #include <string.h>
 
-
+/**
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions.
+ * 
+ */
 int _putchar (char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
